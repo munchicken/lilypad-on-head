7c87a6678cb666b514c8963ee63d56a49ef25864
Sarah Pierce <hacknsnack@yahoo.com>
2018-08-12 00:33:56 -0500

    Added texture

--- src/main/java/com/munchicken/lilypadonhead/ItemLilyPadHat.java
+++ src/main/java/com/munchicken/lilypadonhead/ItemLilyPadHat.java
@@ -4,8 +4,12 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemLilyPadHat extends Item {
+
+    private String name = "lilypadhat";
+
     public ItemLilyPadHat() {
-        setUnlocalizedName(LilypadOnHead.MODID + "_" + "lilypadhat");
+        setUnlocalizedName(LilypadOnHead.MODID + "_" + name);
+        setTextureName(LilypadOnHead.MODID + ":" + name);
         setCreativeTab(CreativeTabs.tabMisc);
     }
 }
